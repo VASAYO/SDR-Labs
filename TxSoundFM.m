56f92f33a58cd78b3249e77c539a4df40ea4cf01
VASAYO <vasilevs2003@yandex.ru>
2025-09-23 16:17:28 +0300

    Matlab code done

--- TxSoundFM.m
+++ TxSoundFM.m
@@ -3,10 +3,44 @@
     clc; clear; close all;
 
 % Считываем аудиофайл
-    [RawData, Fs] = audioread("C:\Users\user\Downloads\Source_sound (online-audio-converter.com).mp3");
+    [RawData, Fs] = audioread("C:\Users\user\Documents\SDR-Labs\" + ...
+        "Source_sound.mp3");
 
-% Формируем комплексную огибающую
+% Преобразуем в моно
+    DataMono = sum(RawData, 2)/2;
+    clear RawData;
 
+% Обрезаем по времени 
+    t = (0:length(DataMono)-1)/Fs;
+    DataCut = DataMono(t >= 15 & t < 25);
+    clear t DataMono;
+
+% Модулируем звуковой сигнал
+    FMModObj = comm.FMBroadcastModulator( ...
+        "AudioSampleRate", Fs, ...
+        "SampleRate", 200e3, ...
+        "FrequencyDeviation", 75e3);
+    IQRaw = FMModObj(DataCut);
+    clear DataCut
+
+% Повышаем частоту дискретизации
+    ResampFactor = 10e6/200e3;
+    IQResamp = ResamplingFun(IQRaw, 200e3, 10e6);
+    clear IQRaw
+
+% Масштабируем по уровню 
+    RefLevel = 127;
+    ScaleFactor = RefLevel / ...
+        max([max(abs(real(IQResamp))) max(abs(imag(IQResamp)))]);
+    
+    IQScaled = IQResamp * ScaleFactor;
+    clear IQResamp
 
 % Формируем .bin файл с отсчётами сигнала
+    IQPrepared4Tx = [real(IQScaled), imag(IQScaled)].';
 
+    fid = fopen("C:\Users\user\Documents\SDR-Labs\TxData.bin", "w");
+    fwrite(fid, IQPrepared4Tx(:), "int8");
+    fclose(fid);
+
+    clear IQScaled
