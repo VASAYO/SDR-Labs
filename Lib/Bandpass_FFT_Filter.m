f52bc5e93e16b4f5b49cefaa9ef811d2f18740f0
ans.vasilev <vasilevs2003@yandex.ru>
2025-10-06 12:39:12 +0300

    [-] Lib/Bandpass_FFT_Filter.m

--- Lib/Bandpass_FFT_Filter.m
+++
@@ -1,7 +0,0 @@
-function Out = Bandpass_FFT_Filter(In, Fs, CentralF, BW)
-
-fft_tmp = fftshift(fft(In));
-f = Generete_Freq_Axis_FFT(length(fft_tmp), Fs);
-fft_tmp(abs(f - CentralF) > BW/2 & abs(f + CentralF) > BW/2) = 0;
-
-Out = ifft(ifftshift(fft_tmp));
